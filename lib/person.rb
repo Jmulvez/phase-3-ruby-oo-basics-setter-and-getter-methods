e9461afecde94733cbe00bac93c8b5112000adae
John <johnmulvey48@yahoo.com>
2022-07-26 13:52:42 -0400

    commit tree 3c4a7470577ccf1823f260e32f2f091070e4ed4b

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def jobs=(jobs)
+        @jobs = jobs
+    end
+    def jobs
+        @jobs
+    end
 end
